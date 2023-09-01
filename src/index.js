9deab3181b117e17c4eef2d20d7cbf07a6e20c26
Thomas Spoelhof <thomasjspoelhof@gmail.com>
2023-08-31 23:14:46 -0400

    to do list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskList = new TaskList();
+  const newTask = document.getElementById("tasks");
+
+  const renderApp = () => (taskUl.innerHTML = taskList.renderTasks());
+
+  newForm.addEventListener("submit", (eventFunc) => {
+    eventFunc.preventDefault();
+    taskList.createNewTask(newTaskDescription.value);
+    eventFunc.target.reset();
+    renderApp();
+  //had some trouble getting the code to work in the browser, but feel like I'm getting close.
+  // *Reminder for myself to return to this assignment later*
+  })
 });
+
